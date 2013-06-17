c829901c19c34a5ef8fa7109bb651705560d98a8
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2013-06-18 00:03:08 +0200

    Now recoded from rastamat:  - powspec  - audspec  - fft2barkmx  - hz2bark  - fft2melmx  - hz2mel  - mel2hz  - postaud  - dolpc  - lpc2cep  - spec2cep  - lifter  - mfcc (melfcc) recoded from octave signal processing  - hamming  - hanning  - specgram  - levinson  - toeplitz
    Tested that mfcc.jl gives same answer for x=[1:1000] and HTK params...

--- fea/mfcc.jl
+++ fea/mfcc.jl
@@ -1,9 +1,29 @@
-function mfcc(x::Vector; sr=16000.0, wintime=0.025, steptime=0.01, numcep=13, lifterexp=-22, 
-              sumpower=false, preemph=0.97, dither=false, minfreq=0.0, maxfreq=8000.0, 
+function mfcc(x::Vector, sr::Number=16000.0; wintime=0.025, steptime=0.01, numcep=13, lifterexp=-22, 
+              sumpower=false, preemph=0.97, dither=false, minfreq=0.0, maxfreq=sr/2, 
               nbands=20, bwidth=1.0, dcttype=3, fbtype="htkmel", usecmp=false, modelorder=0)
     if (preemph!=0) 
         x = x | Filter([1, -preemph])
     end
     pspec = powspec(x, sr, wintime=wintime, steptime=steptime, dither=dither)
-    aspec = audspec(pspec, sr, nbands, fbtype, minfreq, maxfreq, sumpower, bwith)
+    println(pspec)
+    aspec = audspec(pspec, sr, nfilts=nbands, fbtype=fbtype, minfreq=minfreq, maxfreq=maxfreq, sumpower=sumpower, bwidth=bwidth)
+    println(aspec)
+    if usecmp
+        #  PLP-like weighting/compression
+        aspec = postaud(aspec, maxfreq, fbtype)
+    end
+    if modelorder>0
+        if dcttype != 1
+            ## error
+        end
+        # LPC analysis 
+        lpcas = dolpc(aspectrum, modelorder)
+        # cepstra
+        cepstra = lpc2cep(lpcas, numcep)
+    else
+        cepstra = spec2cep(aspec, numcep, dcttype)
+    end
+    cepstra = lifter(cepstra, lifterexp)
+    return cepstra
 end
+
