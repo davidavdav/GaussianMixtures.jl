a85a2a8ef06fbe86d1cdb6fc25f97a748631c66d
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2013-10-29 11:01:06 +0100

    type Filter now in "filtertype.jl"

---
+++ fea/filtertype.jl
@@ -0,0 +1,24 @@
+## This should not be reloaded
+
+type Filter
+    a::Vector
+    b::Vector
+    c::Vector
+    d::Vector
+    xhist::Vector
+    yhist::Vector
+    function Filter(a::Vector, b::Vector)
+        xhist=zeros(length(b)-1)
+        yhist=zeros(length(a)-1)
+        c = reverse(a[2:])/a[1]
+        d = reverse(b)/a[1]
+        new(a, b, c, d, xhist, yhist)
+    end
+end
+Filter(a::Number, b::Vector) = Filter([a], b)
+Filter(a::Vector, b::Number) = Filter(a, [b])
+Filter(b::Vector) = Filter([1.0], b)
+
+import Base.copy
+copy(f::Filter) = Filter(f.a, f.b)
+
