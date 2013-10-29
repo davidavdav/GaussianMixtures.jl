f3edef5402b903de4dcbfc36513b9d69c5a02a43
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2013-10-29 10:57:47 +0100

    - removed types  - add exception for N=0, because implementation is extremely slow  - added multichannel support

--- fea/filter.jl
+++ fea/filter.jl
@@ -1,42 +1,38 @@
 ## This function computes the response of an IIR with coefficients b and a, 
 ## for a signal x
-##module SignalProcessing
+
+## it is extremely inefficient if a is given (i.e., an iir)...
+
+## see the definition of Filter which is in filtertype.jl for practical reasons
 
 export Filter, filter
-
-type Filter
-    c::Vector
-    d::Vector
-    xhist::Vector
-    yhist::Vector
-    function Filter(a::Vector, b::Vector)
-        xhist=zeros(length(b)-1)
-        yhist=zeros(length(a)-1)
-        c = reverse(a[2:])/a[1]
-        d = reverse(b)/a[1]
-        new(c, d, xhist, yhist)
-    end
-end
-Filter(a::Number, b::Vector) = Filter([a], b)
-Filter(a::Vector, b::Number) = Filter(a, [b])
-Filter(b::Vector) = Filter([1.0], b)
 
 import Base.filter
 
 function filter(x::Vector, f::Filter) 
     N = length(f.c)             # length of yhist
     M = length(f.d)-1           # length of xhist
-    y = vcat(f.yhist, similar(x))
-    xx = vcat(f.xhist, x)
-    for n=1:length(x)
-        y[n+N] = -sum(f.c .* y[n:n+N-1]) + sum(f.d .* xx[n:n+M])
+    y = vcat(f.yhist, zero(x))  # N y history
+    xx = vcat(f.xhist, x)       # M x history
+    if N==0 
+        ## xx[1+M] is the first x valus
+        ## f.d[1+M] is the first coefficient, for the currect x[i], 
+        ## f.d[1] is the last coefficient, for the delayed x[i-M]
+        for i=1:M+1
+            y += f.d[i]*xx[i:end-M-1+i] 
+        end
+    else                        # slow implementation
+        for n=1:length(x)
+            y[n+N] = -sum(f.c .* y[n:n+N-1]) + sum(f.d .* xx[n:n+M])
+        end
     end
     f.xhist=x[end-M+1:]
     f.yhist=y[end-N+1:]
     return(y[N+1:])
 end
-
 |(x::Vector, f::Filter) = filter(x, f)
 
+## Array generalization, possibly in parallel
+filter(x::Array, f::Filter) = @parallel (hcat) for i=1:size(x,2) x[:,i] | copy(f) end
+|(x::Array, f::Filter) = filter(x, f)
 ##end
-                                                              
