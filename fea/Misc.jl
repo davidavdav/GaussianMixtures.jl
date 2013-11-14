cec27dbd9ad7c9ec750fc0224bbc31334669db05
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2013-11-14 10:03:45 +0100

    added znorm, znorm!

--- fea/Misc.jl
+++ fea/Misc.jl
@@ -2,10 +2,13 @@
 
 module Misc
 
-export nrow, ncol, sortperm
+export nrow, ncol, sortperm, znorm, znorm!
 
 nrow(x::Array) = size(x,1)
 ncol(x::Array) = size(x,2)
+
+znorm(x::Array, dim::Int=1) = broadcast(/, broadcast(-, x, mean(x, dim)), std(x, dim))
+znorm!(x::Array, dim::Int=1) = broadcast!(/, x, broadcast!(-, x, x, mean(x, dim)), std(x, dim))
 
 import Base.Sort.sortperm
 
