4763b5705c2a7f856be26ee051bcda58b5ad31a3
David van Leeuwen <david.vanleeuwen@gmail.com>
2013-11-15 15:57:01 +0100

    new parameters to feacalc, new feacalc, defining default features for speaker, language, diarization augment, normalization and sad types are now available

--- fea/Feacalc.jl
+++ fea/Feacalc.jl
@@ -11,7 +11,7 @@ using Rasta
 using Misc
 
 ## if test==true, compute features with parmeters as we use in speaker recogntion
-function feacalc(wavfile::String; dynrange::Float64=30., defaults=:spkid_toolkit)
+function feacalc(wavfile::String; augtype=:ddelta, normtype=:warp, sadtype=:energy, defaults=:spkid_toolkit, dynrange::Real=30., nwarp::Int=399)
     (x, sr) = wavread(wavfile)
     sr = convert(Float64, sr)       # more reasonable sr
     data = {"nx" => nrow(x), "sr" => sr, "source" => wavfile} # save some metadata
@@ -23,22 +23,61 @@ function feacalc(wavfile::String; dynrange::Float64=30., defaults=:spkid_toolkit
     (m, pspec, meta) = mfcc(x, sr, defaults)
     data["nftot"] = nrow(m)
  
-    ## integrate power
-    deltaf = size(pspec,2) / (sr/2)
-    minfreqi = iround(300deltaf)
-    maxfreqi = iround(4000deltaf)
-    power = 10log10(sum(pspec[:,minfreqi:maxfreqi], 2))
+    ## augment features
+    if augtype==:delta || augtype==:ddelta
+        d = deltas(m)
+        if augtype==:ddelta
+            dd = deltas(d)
+            m = hcat(m, d, dd)
+        else 
+            m = hcat(m, d)
+        end
+    elseif augtype==:sdc
+        m = sdc(m)
+    end
+    data["augtype"] = string(augtype)
+
+    if sadtype==:energy
+        ## integrate power
+        deltaf = size(pspec,2) / (sr/2)
+        minfreqi = iround(300deltaf)
+        maxfreqi = iround(4000deltaf)
+        power = 10log10(sum(pspec[:,minfreqi:maxfreqi], 2))
     
-    maxpow = maximum(power)
-    speech = find(power .> maxpow - dynrange)
-    
+        maxpow = maximum(power)
+        speech = find(power .> maxpow - dynrange)
+        meta["dynrange"] = dynrange
+    elseif sadtype==:none
+        speech = 1:nrow(m)
+    end
+    data["sadtype"] = string(sadtype)
+    ## perform SAD
     m = m[speech,:]
-    m = warp(m)
     data["speech"] = convert(Vector{Uint32}, speech)
     data["nf"] = nrow(m)
-    meta["dynrange"] = dynrange
-    meta["warp"] = 399          # the default
+    
+    ## normalization
+    if normtype==:warp
+        m = warp(m, nwarp)
+        meta["warp"] = nwarp          # the default
+    elseif normtype==:mvn
+        znorm!(m,1)
+    end
+    data["normtype"] = string(normtype)
+
     return(convert(Array{Float32},m), data, meta)
+end
+
+function feacalc(wavfile::String, application::Symbol)
+    if (application==:speaker)
+        feacalc(wavfile; defaults=:spkid_toolkit)
+    elseif (application==:language)
+        feacalc(wavfile; defaults=:rasta, warp=299, augtype=:sdc)
+    elseif (application==:diarization)
+        feacalc(wavfile; defaults=:rasta, sadtype=:none, normtype=:mvn, augtype=:none)
+    else
+        error("Unknown application ", application)
+    end
 end
 
 function sad(pspec::Array{Float64,2}, sr::Float64, method=:energy; dynrange::Float64=30.)
