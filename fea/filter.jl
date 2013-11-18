a41878272aaa5875957d8cd999d68d18bc2ae95f
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2013-11-18 08:57:53 +0100

    - made filter() type aware, and threrfore a lot faster  - removed vector implementation (loop is faster)

--- fea/filter.jl
+++ fea/filter.jl
@@ -9,31 +9,26 @@ export Filter, filter
 
 import Base.filter
 
-function filter{T<:Real}(x::Vector{T}, f::Filter) 
+function filter{T<:Real}(x::Vector{T}, f::Filter{T})
     N = length(f.c)             # length of yhist
     M = length(f.d)-1           # length of xhist
     y = vcat(f.yhist, zero(x))  # N y history
     xx = vcat(f.xhist, x)       # M x history
-    if N==0 
-        ## xx[1+M] is the first x valus
-        ## f.d[1+M] is the first coefficient, for the currect x[i], 
-        ## f.d[1] is the last coefficient, for the delayed x[i-M]
-        for i=1:M+1
-            y += f.d[i]*xx[i:end-M-1+i] 
-        end
-    else                        # slow implementation
-        for n=1:length(x)
-            y[n+N] = -sum(f.c .* y[n:n+N-1]) + sum(f.d .* xx[n:n+M])
-        end
+    s::T = 0
+    for n=1:length(x)
+        s=0
+        for j=1:N s -= f.c[j]*y[n-1+j] end
+        for j=1:M+1 s += f.d[j]*xx[n-1+j] end
+        y[n+N] = s
     end
     f.xhist=x[end-M+1:]
     f.yhist=y[end-N+1:]
     return(y[N+1:])
 end
-|{T<:Real}(x::Vector{T}, f::Filter) = filter(x, f)
+|{T<:Real}(x::Vector{T}, f::Filter{T}) = filter(x, f)
 
 ## Array generalization, possibly in parallel
-filter{T<:Real}(x::Array{T}, f::Filter) = @parallel (hcat) for i=1:size(x,2) x[:,i] | copy(f) end
-|{T<:Real}(x::Array{T}, f::Filter) = filter(x, f)
+filter{T<:Real}(x::Array{T}, f::Filter{T}) = @parallel (hcat) for i=1:size(x,2) x[:,i] | copy(f) end
+|{T<:Real}(x::Array{T}, f::Filter{T}) = filter(x, f)
 
 ##end
