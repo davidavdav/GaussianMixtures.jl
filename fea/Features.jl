a74dc02577322ab122a498aa7b40a66581271fb7
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2013-11-18 13:26:56 +0100

    stricter parameter types

--- fea/Features.jl
+++ fea/Features.jl
@@ -13,11 +13,11 @@ using Misc
 ## i.e., multichannel data
 ## Recoded from rastamat's "melfcc.m" (c) Dan Ellis. 
 ## Defaults here are HTK parameters, this is contrary to melfcc
-function mfcc(x::Vector, sr::Number=16000.0; wintime=0.025, steptime=0.01, numcep=13, 
+function mfcc{T<:FloatingPoint}(x::Vector{T}, sr::FloatingPoint=16000.0; wintime=0.025, steptime=0.01, numcep=13, 
               lifterexp=-22, sumpower=false, preemph=0.97, dither=false, minfreq=0.0, maxfreq=sr/2,
               nbands=20, bwidth=1.0, dcttype=3, fbtype="htkmel", usecmp=false, modelorder=0)
     if (preemph!=0) 
-        x |= Filter([1, -preemph])
+        x |= Filter([1., -preemph])
     end
     pspec = powspec(x, sr, wintime=wintime, steptime=steptime, dither=dither)
     aspec = audspec(pspec, sr, nfilts=nbands, fbtype=fbtype, minfreq=minfreq, maxfreq=maxfreq, sumpower=sumpower, bwidth=bwidth)
@@ -44,10 +44,10 @@ function mfcc(x::Vector, sr::Number=16000.0; wintime=0.025, steptime=0.01, numce
             "usecmp" => usecmp, "modelorder" => modelorder}
     return (cepstra, pspec', meta)
 end
-mfcc(x::Array, sr::Number=16000.0...) = @parallel (tuple) for i=1:size(x)[2] mfcc(x[:,i], sr...) end
+mfcc{T<:FloatingPoint}(x::Array{T}, sr::FloatingPoint=16000.0...) = @parallel (tuple) for i=1:size(x)[2] mfcc(x[:,i], sr...) end
 
 ## default feature configurations, :rasta, :htk, :spkr_toolkit
-function mfcc(x::Vector, sr::Number, defaults::Symbol) 
+function mfcc{T<:FloatingPoint}(x::Vector{T}, sr::FloatingPoint, defaults::Symbol) 
     if defaults==:rasta
         mfcc(x, sr; lifterexp=0.6, sumpower=true, nbands=40, dcttype=2, fbtype="mel")
     elseif defaults==:spkid_toolkit
@@ -58,17 +58,17 @@ function mfcc(x::Vector, sr::Number, defaults::Symbol)
 end
 
 ## our features run down with time
-function deltas(x, w::Int=9)
+function deltas{T<:FloatingPoint}(x::Array{T}, w::Int=9)
     (nr, nc) = size(x)
     hlen = ifloor(w/2)
     w = 2hlen+1                 # make w odd
-    win = [hlen:-1:-hlen]
+    win = [convert(T,hlen):-1:-hlen]
     xx = vcat(repmat(x[1,:], hlen, 1), x, repmat(x[end,:], hlen, 1))
     return (xx | Filter(win))[2hlen+(1:nr),:]
 end
 
 
-function warp(x::Array, w=399)
+function warp{T<:FloatingPoint}(x::Array{T}, w=399)
     l = nrow(x)
     wl = min(w, l)
     hw = iround((wl+1)/2)
@@ -94,7 +94,7 @@ function warp(x::Array, w=399)
     return erfinvtab[rank]        
 end
 
-function sdc(x::Array, n::Int=7, d::Int=1, p::Int=3, k::Int=7)
+function sdc{T<:FloatingPoint}(x::Array{T}, n::Int=7, d::Int=1, p::Int=3, k::Int=7)
     (nx, nfea) = size(x)
     @assert n <= nfea
     xx = vcat(delta(x[:,1:n], d), zeros(typeof(x[1]), (k-1)*p, n))
