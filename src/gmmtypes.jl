2192c623e75de32a7ef5358ebe0efadcfe1cc0e4
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2014-09-23 08:18:13 +0200

    Removed Data type, is now in BigData

--- src/gmmtypes.jl
+++ src/gmmtypes.jl
@@ -103,14 +103,3 @@ IExtractor{T}(Tee::Matrix{T}, prec::Vector{T}) = IExtractor{T}(Tee, prec)
 ## or initialize with a traditional covariance matrix
 IExtractor{T}(Tee::Matrix{T}, Σ::Matrix{T}) = IExtractor{T}(Tee, vec(1./Σ'))
 
-## A data handle, either in memory or on disk, perhaps even mmapped but I haven't seen any 
-## advantage of that.  It contains a list of either files (where the data is stored)
-## or data units.  The point is, that in processing, these units can naturally be processed
-## independently.  
-type Data
-    datatype::Type
-    list::Vector
-    read::Union(Function,Nothing)
-end
-
-typealias DataOrMatrix Union(Data, Matrix)
