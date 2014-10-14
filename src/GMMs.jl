e46f731d1bc6ea6cdcbf0007237a54969b742481
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2014-10-14 22:01:30 +0200

    using Distributions, less export

--- src/GMMs.jl
+++ src/GMMs.jl
@@ -9,17 +9,19 @@ ccall(:jl_zero_subnormals, Bool, (Bool,), true)
 using NumericExtensions
 using BigData
 using Distributions
+using Clustering
 using HDF5, JLD
 using MAT
 
 include("gmmtypes.jl")
 
 include("gmms.jl")
+include("train.jl")
 include("io.jl")
 include("stats.jl")
 include("rand.jl")
 include("recognizer.jl")
 
-export GMM, CSstats, Stats, IExtractor, History, Data, DataOrMatrix, split, em!, map, llpg, posterior, history, show, stats, cstats, savemat, readmat, nparams, means, covars, weights, setmem, vec, rand, ivector
+export GMM, CSstats, Stats, IExtractor, History, split, em!, map, llpg, posterior, history, show, stats, cstats, savemat, readmat, nparams, means, covars, weights, setmem, vec, rand, ivector
 
 end
