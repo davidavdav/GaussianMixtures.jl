ef5a935b02581644f926c65dc7d05a638cbabd7a
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2014-10-17 15:27:03 +0200

    Stats -> Cstats rename

--- src/GMMs.jl
+++ src/GMMs.jl
@@ -22,6 +22,6 @@ include("stats.jl")
 include("rand.jl")
 include("recognizer.jl")
 
-export GMM, CSstats, Stats, History, split, em!, map, llpg, avll, posterior, history, show, stats, readmat, nparams, means, covars, weights, setmem, vec, rand
+export GMM, CSstats, Cstats, History, split, em!, map, llpg, avll, posterior, history, show, stats, readmat, nparams, means, covars, weights, setmem, vec, rand
 
 end
