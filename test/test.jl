b0856200f49c0ce8f2e7f5d5971c57bf35e1f8a1
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2014-10-24 10:07:37 +0200

    Add av ll computation from stats

--- test/test.jl
+++ test/test.jl
@@ -11,10 +11,11 @@ for (gmmkind, Ng) in zip((:diag, :full), (256, 16))
     ## do some computations
     st = stats(gmm, x)
     println("nx: ", size(x,1), " sum(zeroth order stats): ", sum(st[3]))
+    println("avll from stats: ", st[2] / length(x))
     ll = llpg(gmm, x)
-    println("avll from llpg: ", sum(log(exp(ll) * gmm.w)) / length(x))
+    println("avll from llpg:  ", sum(log(exp(ll) * gmm.w)) / length(x))
     av = avll(gmm, x)
-    println("avll direct: ", av)
+    println("avll direct:     ", av)
     p = posterior(gmm, x)
     println("sum posterior: ", sum(p))
 end
@@ -23,6 +24,7 @@ end
 gmm = rand(GMM, 32, 26, sep=0.1, kind=:diag)
 x = rand(gmm, 100000)
 g2 = GMM(32, x, nIter=50)
+## what is the point of displaying?
 display(means(gmm))
 display(means(g2))
 
