0bff11a6b4383a7e8a94f2b168d73da5c7b78de6
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2014-10-31 23:06:50 +0100

    Add some tests for variational Bayes

---
+++ test/testvb.jl
@@ -0,0 +1,14 @@
+## test VB GMM for the standard example data
+x = readdlm("test/faithful.txt")
+## only do k-means
+g = GMM(8, x, kind=:full, nIter=0)
+p = GMMprior(g.d, 1.0, 1.0)
+v = VGMM(g, p)
+em!(v, x)
+## show the reduction in Gaussians
+println(history(v))
+## we have no pretty printing yet...
+for f in [:α, :β, :m, :ν, :W] 
+    println(f, " = ", getfield(v, f))
+end
+    
