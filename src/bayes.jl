274ea234adc4a7693eae16194f30e420a6401dae
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2014-10-31 22:49:15 +0100

    First sort-of working version of variational Bayes, include prior in VGMM, reduce # gaussians

--- src/bayes.jl
+++ src/bayes.jl
@@ -12,24 +12,13 @@ require("bayestypes.jl")
 function GMMprior{T<:FloatingPoint}(d::Int, alpha::T, beta::T)
     m0 = zeros(T, d)
     W0 = eye(T, d)
-    nu0 = convert(T,d)
-    GMMprior(alpha, beta, m0, W0, nu0)
+    ν0 = convert(T,d)
+    GMMprior(alpha, beta, m0, W0, ν0)
 end
+Base.copy(p::GMMprior) = GMMprior(p.α0, p.β0, copy(p.m0), copy(p.W0), p.ν0)
 
-## not sure this constructor is at all useful.
-function VGMM(ng::Int, prior::GMMprior)
-    d = length(prior.m0)
-    alpha = prior.α0 * ones(ng)
-    beta = prior.β0 * ones(ng)
-    m = repmat(prior.m0', ng)
-    W = [prior.W0 for k=1:ng]
-    nu = prior.ν0 * ones(ng)
-    hist = History(@sprintf("VGMM with %d Gaussians in %d dimensions initialized from prior", ng, d))
-    VGMM(ng, d, :full, alpha, beta, m, nu, W, [hist])
-end
-
-## initialize from a GMM and data, seems we only need nx, perhaps store that in GMM?
-function VGMM(g::GMM, prior::GMMprior)
+## initialize from a GMM and nx, the number of points used to train the GMM.
+function VGMM(g::GMM, π::GMMprior)
     nx = g.nx
     N = g.w * nx
     mx = g.μ
@@ -38,10 +27,10 @@ function VGMM(g::GMM, prior::GMMprior)
     else
         S = g.Σ
     end
-    α, β, m, nu, W = mstep(prior, N, mx, S)
+    α, β, m, ν, W, keep = mstep(π, N, mx, S)
     hist = copy(g.hist)
     push!(hist, History("GMM converted to Varitional GMM"))
-    VGMM(g.n, g.d, :full, α, β, m, nu, W, hist)
+    VGMM(g.n, g.d, π, α, β, m, ν, W, hist)
 end
 
 ## sharpen VGMM to a GMM
@@ -51,7 +40,7 @@ function GMM(v::VGMM)
     μ = v.m
     Σ = similar(v.W)
     for k=1:length(v.W)
-        Σ[k] = inv(v.nu[k] * v.W[k])
+        Σ[k] = inv(v.ν[k] * v.W[k])
     end
     hist = copy(v.hist)
     push!(hist, History("Variational GMM converted to GMM"))
@@ -59,26 +48,29 @@ function GMM(v::VGMM)
 end
 
 ## m-step given prior and stats
-function mstep(prior::GMMprior, N, mx, S)
-    α = prior.α0 + N           # ng, 10.58
-    nu = prior.ν0 + N + 1      # ng, 10.63
-    β = prior.β0 + N           # ng, 10.60
+function mstep(π::GMMprior, N, mx, S)
+    ng = length(N)
+    α = π.α0 + N           # ng, 10.58
+    ν = π.ν0 + N + 1      # ng, 10.63
+    β = π.β0 + N           # ng, 10.60
     m = similar(mx)            # ng * d
     W = similar(S)             # ng * (d*d)
     d = size(mx,2)
     limit = sqrt(eps(eltype(N)))
-    for k=1:g.n
+    keep = trues(length(N))
+    for k=1:ng
         if N[k] > limit
-            m[k,:] = (prior.β0*prior.m0' + N[k]*mx[k,:]) ./ β[k] # 10.61
-            Δ = mx[k,:] - prior.m0'
-            third = prior.β0 * N[k] / (prior.β0 + N[k]) * Δ' * Δ
-            W[k] = inv(inv(prior.W0) + N[k]*S[k] + third) # 10.62
+            m[k,:] = (π.β0*π.m0' + N[k]*mx[k,:]) ./ β[k] # 10.61
+            Δ = mx[k,:] - π.m0'
+            third = π.β0 * N[k] / (π.β0 + N[k]) * Δ' * Δ
+            W[k] = inv(inv(π.W0) + N[k]*S[k] + third) # 10.62
         else
+            keep[k] = false
             m[k,:] = zeros(d)
             W[k] = eye(d)
         end
     end
-    return α, β, m, nu, W
+    return α, β, m, ν, W, keep
 end
 
 ## log(ρ_nk) from 10.46, start with a very slow implementation
@@ -90,13 +82,13 @@ function logρ(g::VGMM, x::Matrix)
     Elogπ = digamma(g.α) .- digamma(sum(g.α)) # 10.66, size ng 
     ElogdetΛ = similar(Elogπ) # size ng
     for k=1:ng
-        ElogdetΛ[k] = sum(digamma(0.5(g.nu[k] .+ 1 .- [1:d]))) .+ d*log(2) .+ logdet(g.W[k]) # 10.65
+        ElogdetΛ[k] = sum(digamma(0.5(g.ν[k] .+ 1 .- [1:d]))) .+ d*log(2) .+ logdet(g.W[k]) # 10.65
     end
     EμkΛk = similar(x, nx, ng)
     for i=1:nx
         for k=1:ng
             xx = x[i,:] - g.m[k,:]
-            EμkΛk[i,k] = d/g.β[k] + g.nu[k]* dot(xx*g.W[k], xx)
+            EμkΛk[i,k] = d/g.β[k] + g.ν[k]* dot(xx*g.W[k], xx)
         end
     end
     broadcast(+, (Elogπ + 0.5ElogdetΛ .- 0.5d*log(2π))', -0.5EμkΛk)
@@ -132,34 +124,45 @@ function threestats(g::VGMM, x::Matrix)
 end
 
 ## do exactly one update step for the VGMM
-function emstep!(g::VGMM, x::Matrix, prior::GMMprior)
-    N, mx, S = threestats(g, x, prior)
-    g.α, g.β, g.m, g.nu, g.W = mstep(prior, N, mx, S)
+function emstep!(g::VGMM, x::Matrix)
+    N, mx, S = threestats(g, x)
+    g.α, g.β, g.m, g.ν, g.W, keep = mstep(g.π, N, mx, S)
+    n = sum(keep)
+    if n<g.n
+        ## only keep useful Gaussians...
+        for f in [:α, :β, :ν, :W]
+            setfield!(g, f, getfield(g, f)[keep])
+        end
+        g.m = g.m[keep,:]
+        g.n = n
+        addhist!(g, string("dropping number of Gaussions to ",n))
+    end
     g
 end
 
-function em!(g::VGMM, x::Matrix, prior::GMMprior; nIter=50)
+function em!(g::VGMM, x::Matrix; nIter=50)
     for i=1:nIter
-        emstep!(g, x, prior)
+        emstep!(g, x)
     end
+    addhist!(g, string(nIter, " variational Bayes EM-like iterations"))
 end
 
 ## Not used
 
-## Wishart distribution {\cal W}(Λ, W, ν), but we write nu for ν. 
-function Wishart(Λ::Matrix, W::Matrix, nu::Float64)
+## Wishart distribution {\cal W}(Λ, W, ν).
+function Wishart(Λ::Matrix, W::Matrix, ν::Float64)
     ## check
     issym(W) || error("W must be symmetric")
     d = size(W,1)
     (d,d) == size(Λ) || error("Λ must be same size as W")
     ## norm
-    B = det(W)^(-0.5nu) / (2^(0.5nu*d) * π^(0.25d*(d-1)))
+    B = det(W)^(-0.5ν) / (2^(0.5ν*d) * π^(0.25d*(d-1)))
     for i=1:d
-        B /= gamma(0.5(nu+1-i))
+        B /= gamma(0.5(ν+1-i))
     end
 #    invΛ = inv(Λ)
     ex = -0.5dot(inv(W),Λ)
-    B * det(Λ)^(0.5(nu-d-1)) * exp(ex)
+    B * det(Λ)^(0.5(ν-d-1)) * exp(ex)
 end
 
 function Gaussian(x::Vector, μ::Vector, Σ::Matrix)
@@ -171,7 +174,7 @@ function Gaussian(x::Vector, μ::Vector, Σ::Matrix)
     norm * exp(ex)
 end
 
-function GaussianWishart(μ::Vector, Λ::Matrix, μ0::Vector, β::Float64, W::Matrix, nu::Float64)
-    Gaussian(μ, μ0, inv(βΛ)) * Wishart(Λ, W, nu)
+function GaussianWishart(μ::Vector, Λ::Matrix, μ0::Vector, β::Float64, W::Matrix, ν::Float64)
+    Gaussian(μ, μ0, inv(βΛ)) * Wishart(Λ, W, ν)
 end 
 
