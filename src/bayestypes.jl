2ef0d505f351e35aa30e99922e1fd61f8fde57d1
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2014-10-31 22:51:17 +0100

    Use ν not nu, include prior in VGMM, remove kind

--- src/bayestypes.jl
+++ src/bayestypes.jl
@@ -1,25 +1,26 @@
 ## experiments with variational Bayes
 
+## Please note our pedantic use of the Greek letter ν, don't confuse this with v!
 type GMMprior{T<:FloatingPoint}
-    α0::T                     # effective prior number of observations
+    α0::T                       # effective prior number of observations
     β0::T
     m0::Vector{T}               # prior on μ
     W0::Matrix{T}               # prior precision
-    ν0::T                       # idem
+    ν0::T                       # scale precision
 end    
 
 ## In Variational Bayes, the GMM is not specified by point estimates of the paramters, but distributions
 ## over these parameters.  
 ## These are Dirichlet for the weights and Gaussian-Wishart for the mean and precision.  
 ## These distributions have parameters themselves, and these are stored in the type...
-type VGMM{T<:FloatingPoint}
+type VGMM{T<:FloatingPoint} <: GaussianMixture{T}
     n::Int                      # number of Gaussians
     d::Int                      # dimension of Gaussian
-    kind::Symbol                # :diag or :full---we'll take 'diag' for now
+    π::GMMprior                 # The prior used in this VGMM
     α::Vector{T}                # Dirichlet, n
     β::Vector{T}                # scale of precision, n
     m::Matrix{T}                # means of means, n * d
-    nu::Vector{T}               # no. degrees of freedom, n
+    ν::Vector{T}                # no. degrees of freedom, n
     W::Vector{Matrix{T}}        # scale matrix for precision? n * d * d
     hist::Vector{History}       # history
 end
